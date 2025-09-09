19250fd8e0713e9b2397ce8b602df4a103685603
phanminhtri667 <phanminhtri667@gmail.com>
2025-09-09 14:43:46 +0700

    seed chấm công

---
+++ server/src/routers/timekeepingRouter.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import * as timekeepingController from '../controllers/timekeepingController';
+import { verifyToken } from '../middlewares/verify_token';
+
+const router = express.Router();
+
+router.use(verifyToken);
+
+router.post('/clock-in', timekeepingController.clockIn);
+router.post('/clock-out', timekeepingController.clockOut);
+router.get('/my', timekeepingController.getMyTimekeeping);
+
+export default router;
